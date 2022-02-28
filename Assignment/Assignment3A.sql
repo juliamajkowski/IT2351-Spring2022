03c8afa70f4899e00c4f3d56f6f2df1e01d4aaf2
Julia Majkowski <77689607+juliamajkowski@users.noreply.github.com>
2022-02-28 14:15:28 -0500

    Delete Assignment directory

--- Assignment/Assignment3A.sql
+++
@@ -1,5 +0,0 @@
--- Purpose create a query that that returns these columns: The count of the number of orders in the orders table and the sum of the “Ship Value” 
--- Julia Majkowski IT2351 02/28/2022
-
-SELECT COUNT(order_id) AS "number of orders", SUM(ship_amount) AS "ship value" FROM orders;
-
--- Assignment/Assignment3B.sql
+++
@@ -1,4 +0,0 @@
--- Purpose create a query that shows the count of products and the maximum product item price
--- Julia Majkowski IT2351 02/28/2022
-
-SELECT COUNT(product_id), MAX(list_price) FROM products;
